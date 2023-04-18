b6628bf41a88aba63c657bb43d91715e4f560309
webiotsanjali <47311351+webiotsanjali@users.noreply.github.com>
2023-04-18 18:55:25 +0530

    about us slider done

--- assets/js/custom-swiper.js
+++ assets/js/custom-swiper.js
@@ -27,3 +27,35 @@ const brand_swiper1 = new Swiper(".brand-slider-1", {
         },
     },
 });
+
+var review_swiper = new Swiper('.review-slider-1', {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true
+    },
+    loop: true,
+    navigation: {
+        prevEl: '.slidePrev-btn',
+        nextEl: '.slideNext-btn'
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 1
+        },
+        992: {
+            slidesPerView: 2
+        },
+        1560: {
+            slidesPerView: 2
+        }
+    }
+});
