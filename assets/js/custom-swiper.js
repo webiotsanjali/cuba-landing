fd834f42208e515e47ae0fdc6252ca4aa970b8a6
webiotsanjali <47311351+webiotsanjali@users.noreply.github.com>
2023-04-25 18:59:36 +0530

    cuba landing 2 completed

--- assets/js/custom-swiper.js
+++ assets/js/custom-swiper.js
@@ -28,7 +28,41 @@ const brand_swiper1 = new Swiper(".brand-slider-1", {
     },
 });
 
-var review_swiper = new Swiper('.review-slider-1', {
+const brand_swiper2 = new Swiper(".brand-slider-2", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 2000,
+    },
+    navigation: {
+        prevEl: '.slidePrev-btn',
+        nextEl: '.slideNext-btn'
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 12,
+        },
+        420: {
+            slidesPerView: 2,
+            spaceBetween: 18,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 18,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 18,
+        },
+        1200: {
+            slidesPerView: 6,
+            spaceBetween: 18,
+        },
+    },
+});
+
+const review_swiper = new Swiper('.review-slider-1', {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
@@ -60,9 +94,33 @@ var review_swiper = new Swiper('.review-slider-1', {
     }
 });
 
+const review_swiper_2 = new Swiper('.review-slider-2', {
+    loop: true,
+    spaceBetween: 30,
+    navigation: {
+        prevEl: '.slidePrev-btn',
+        nextEl: '.slideNext-btn'
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 1
+        },
+        992: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 3
+        }
+    }
+});
+
 const plan_swiper1 = new Swiper(".plan-slider-1", {
     slidesPerView: 3,
     spaceBetween: 25,
+    loop: true,
     autoplay: {
         delay: 2000,
     },
@@ -73,6 +131,33 @@ const plan_swiper1 = new Swiper(".plan-slider-1", {
         },
         530: {
             slidesPerView: 2,
+            spaceBetween: 18,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 18,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+    },
+});
+
+const plan_swiper2 = new Swiper(".plan-slider-2", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 12,
+        },
+        530: {
+            slidesPerView: 1,
             spaceBetween: 18,
         },
         768: {
